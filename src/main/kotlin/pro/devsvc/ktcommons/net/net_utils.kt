9c90072d980f24f91b0a5e31c022385aa32ff8b8
devsvc <open.dev@outlook.com>
2020-08-12 16:33:43 +0800

    add test cases

--- src/main/kotlin/pro/devsvc/ktcommons/net/net_utils.kt
+++ src/main/kotlin/pro/devsvc/ktcommons/net/net_utils.kt
@@ -1,19 +1,17 @@
 package pro.devsvc.ktcommons.net
 
-import java.net.InetSocketAddress
-
+import java.net.DatagramSocket
+import java.net.InetAddress
 import java.net.Socket
+
 
 /**
  * get local ip address
  * @return ip: e.g. 192.168.1.112
  */
 fun getLocalHostIP(): String {
-    val socket = Socket()
-    socket.use {
-        it.connect(InetSocketAddress("google.com", 80))
-        val ip = socket.localAddress.toString().replace("/", "")
-        socket.close()
-        return ip
+    DatagramSocket().use { socket ->
+        socket.connect(InetAddress.getByName("8.8.8.8"), 10002)
+        return socket.localAddress.hostAddress
     }
 }
